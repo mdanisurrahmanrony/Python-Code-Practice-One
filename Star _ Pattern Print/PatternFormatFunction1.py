c24f783def00a8bca36ec3256abe330d4de4c671
Md. Anisur Rahman Rony <52064058+mdanisurrahmanrony@users.noreply.github.com>
2020-01-17 16:24:46 +0600

    Delete PatternFormatFunction1.py

--- Star _ Pattern Print/PatternFormatFunction1.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter the number of rows: "))
-for  i in range(1,num+1):
-    for j in range(1,num-i+1):
-        print(format(" ","<3"),end="")
-    for j in range (i,0,-1):
-        print(format(j,"<3"),end="")
-    for j in range(2,i+1):
-        print(format(j,"<3"),end="")
-    print()
