f30e442c1398f2f08e518d907571aae8f4561299
Md. Anisur Rahman Rony <52064058+mdanisurrahmanrony@users.noreply.github.com>
2020-01-17 16:24:19 +0600

    Delete HollowTriangleStar.1.py

--- Star _ Pattern Print/HollowTriangleStar.1.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter the number of  rows: "))
-for row in range(1,num+1):
-    for col in range(1,2*num):
-        if row==num or row+col==num+1 or col-row==num-1:
-            print("*",end="")
-        else:
-            print(end=" ")
-    print()
